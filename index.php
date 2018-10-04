cbd27448d9249bae831d17a1c6606a0734917339
thomroorda <43601322+thomroorda@users.noreply.github.com>
2018-10-04 13:16:42 +0200

    index.html veranderd naar index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Versiebeheer</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+
+	<body>
+	<div class='backdrop'>
+	<div class='container'>
+	</div>
+	</div>
+	<div class='backdropright'>
+	<div class='container'>
+	</div>
+	</div>
+
+</body>
+
+</html>
