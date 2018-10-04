0aa6d0ee5d2d6b2b60026fb3147e7bd1dcfab1f4
sjihdazi <sjihdazi@gmail.com>
2018-10-04 13:57:24 +0200

    formulier + database connectie + database

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include "db.php"; ?>
+
 <!DOCTYPE HTML>
 
 <html lang="en">
@@ -9,12 +11,37 @@
 
 	<body>
 	<div class='backdrop'>
-	<div class='container'>
-	</div>
+		<div class='container'>
+			<form action="./" method="post"> 
+				<label id="first">Onderwerp:</label><br/>
+				<input type="text" name="onderwerp"><br/>
+
+				<label id="first">Comment</label><br/>
+				<input type="text" name="comment"><br/>
+
+				<label id="first">Gewerkte Uren</label><br/>
+				<input type="text" name="uren"><br/>
+
+				<button type="submit" name="save">save</button>
+			</form>
+		</div>
 	</div>
 	<div class='backdropright'>
-	<div class='container'>
-	</div>
+		<div class='container'>
+			<table>
+			<?php
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    while($row = $result->fetch_assoc()) {
+				        echo "<tr><td>Onderwerp: " . $row["onderwerp"]. "</td></tr><tr><td>Comment: " . $row["comment"]. "</td></tr><tr><td>Gewerkte Uren: " . $row["uren"]. "<hr></td></tr>";
+				    }
+				} else {
+				    echo "0 results";
+				}
+				$conn->close();
+			?>
+			</table>
+		</div>
 	</div>
 
 </body>
